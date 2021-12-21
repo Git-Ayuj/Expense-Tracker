a6469fb366c18b236bd4c6ff679afeb5f3eb7628
AyujPanchal <b19099@students.iitmandi.ac.in>
2021-12-22 01:14:39 +0530

    Rendering Lists & Adding Chart

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -9,15 +9,15 @@ function ExpenseForm(props) {
 
   function titleChangeHandler(event) {
     setEnteredTitle(event.target.value);
-  }
+  };
 
   function amountChangeHandler(event) {
     setEnteredAmount(event.target.value);
-  }
+  };
 
   function dateChangeHandler(event) {
     setEnteredDate(event.target.value);
-  }
+  };
 
   function submitHandler(event) {
     event.preventDefault();
@@ -33,7 +33,7 @@ function ExpenseForm(props) {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
-  }
+  };
 
   return (
     <form onSubmit={submitHandler}>
@@ -68,6 +68,7 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
